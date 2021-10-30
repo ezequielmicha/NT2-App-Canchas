af7847e195fee83a130305227a0c4bd3350b2549
Ezequiel <ezequielmicha@gmail.com>
2021-10-30 16:44:37 -0300

    creacion vistas Maps y SelectDate

--- components/TopTabsNavigator/index.js
+++ components/TopTabsNavigator/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
 import Home from '../../screens/Home';
+import Maps from '../../screens/Maps';
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+
 
 export default function MaterialTopTabNavigator() {
 
@@ -11,6 +13,7 @@ export default function MaterialTopTabNavigator() {
     return (
         <TopTabs.Navigator>
             <TopTabs.Screen name={'Inicio'} component={Home}/>          
+            <TopTabs.Screen name={'Maps'} component={Maps}/>          
         </TopTabs.Navigator>
     );
   }
