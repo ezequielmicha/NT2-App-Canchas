ac092f4abaa6b9883b0b626b077e6f4449107d45
Fernando Pallas <ferpallas@gmail.com>
2021-11-17 16:46:48 -0300

    DateTimePicker en wizard

--- screens/Reserve/SelectDate/index.js
+++ screens/Reserve/SelectDate/index.js
@@ -1,24 +1,71 @@
-import React, { useState } from 'react';
-import { Button, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { defaultFormatUtc } from 'moment';
 
-export default () => {
-    return (
-        <View style={styles.container}>
-            <StatusBar style={'auto'} />
+export default function App() {
+  
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
 
-            <View>
-                <Text> SELECT DATE and TIME </Text>
-            </View>
+  useEffect(() => {
+    console.log('Cambie mi fecha!!!!', date)
+  }, [date])
 
-        </View>     
-    )
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.button}>
+        <Button onPress={showDatepicker} title="Seleccionar fecha" />
+      </View>
+      <View style={styles.button}>
+        <Button onPress={showTimepicker} title="Seleccionar hora" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    //flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+   },
+   button: {
+    marginTop: 100,
+    marginBottom: 200
+   }
+
+   
 });
