a27ff319a9bbc78eb21791fe2626a725670a859e
Shanon Samora <60020892+ShanonSamora@users.noreply.github.com>
2021-11-18 18:27:36 -0300

    Ahora hay iconos en el TopTabNavigator. Hecho con Tomas Loughry

--- components/TopTabsNavigator/index.js
+++ components/TopTabsNavigator/index.js
@@ -1,26 +1,73 @@
-import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+//import { StyleSheet, Text, View } from 'react-native';
 import Home from '../../screens/Home';
 import Maps from '../../screens/Maps';
 import Califications from '../../screens/Califications';
 import Contact from '../../screens/Contact';
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Reserve from '../../screens/Reserve';
-
+import { Ionicons } from "@expo/vector-icons";
 
 
 export default function MaterialTopTabNavigator() {
 
-
     const TopTabs = createMaterialTopTabNavigator()
-  
-    return (
-        <TopTabs.Navigator>
-            <TopTabs.Screen name={'Inicio'} component={Home}/>
-            <TopTabs.Screen name={'Reservas'} component={Reserve}/>
-            <TopTabs.Screen name={'Ubicacion'} component={Maps}/>
-            <TopTabs.Screen name={'Calificaciones'} component={Califications} options={{ title: 'Calificaciones', headerShown: false }}/>
-            <TopTabs.Screen name={'Cuenta'} component={Contact}/>
+    const iconsSize = 25;
+     return (
+        <TopTabs.Navigator
+        screenOptions={{
+            tabBarShowLabel: false
+        }}
+        >
+            <TopTabs.Screen name={"Home"}
+            component={Home}
+            options={{
+                title: "Inicio",
+                tabBarIcon: props => (
+                  <Ionicons name="ios-home" size={iconsSize} color={props.color} />
+                )
+              }}
+            />
+
+           <TopTabs.Screen name={'Reservas'}
+           component={Reserve}
+           options={{
+            title: 'Reservas',
+            headerShown: true,
+            tabBarIcon: props => (
+             <Ionicons name="add-circle" size={iconsSize} color={props.color}/>
+           )
+            }}
+           />
+
+           <TopTabs.Screen name={'Ubicacion'} 
+           component={Maps}
+           options={{
+            title: 'Ubicacion',
+            headerShown: true,
+            tabBarIcon: props => (
+             <Ionicons name="location" size={iconsSize} color={props.color}/>
+           )
+            }}/>
+
+           <TopTabs.Screen name={'Calificaciones'} 
+           component={Califications} 
+           options={{
+               title: 'Calificaciones',
+               headerShown: true,
+               tabBarIcon: props => (
+                <Ionicons name="star" size={iconsSize} color={props.color}/>
+              )
+               }}/>
+
+           <TopTabs.Screen name={'Cuenta'} 
+           component={Contact}
+           options={{ 
+            title: "Cuenta",
+            tabBarIcon: props => (
+              <Ionicons name="person-circle-outline" size={iconsSize} color={props.color}/>
+            )
+          }}/>
         </TopTabs.Navigator>
-    );
-  }
+     )
+}
