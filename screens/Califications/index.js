e36a32b98c1db9050b658dfb6c574c4c3c08a787
TOMAS LOUGHRY <36702458@comunidadort.edu.ar>
2021-11-21 11:21:07 -0300

    Funcionalidad de calificar, en proceso.. Trabajo realizado con Shanon Samora

--- screens/Califications/index.js
+++ screens/Califications/index.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
-import { Button, StatusBar, StyleSheet, Text, TextInput, View, ImageBackground } from 'react-native';
+import { Button, StatusBar, StyleSheet, Text, Image, TextInput, View, ImageBackground } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import starFilledImg from '../../assets/starFilled.png';
+import starCornerImg from '../../assets/starCorner.png';
 
 export default () => {
+
+    const [defaultRating, setdefaultRating] = useState(2);
+    const [maxRating, setmaxRating] = useState([1,2,3,4,5]);
+
+    const starImgFilledUri = Image.resolveAssetSource(starFilledImg).uri;
+    const starImgCornerUri = Image.resolveAssetSource(starCornerImg).uri;
+
+    const CustomRatingBAr = () => {
+        return (
+            <View style={styles.customRatingBarStyle}>
+                {
+                    maxRating.map((item, key) => {
+                        return (
+                            <TouchableOpacity
+                            activeOpacity={0.7}
+                            key={item}
+                            onPress={() => setdefaultRating(item)}
+                            >
+                            <Image
+                            style={styles.starImgStyle}
+                            source={
+                                item <= defaultRating
+                                ? {uri: starImgFilledUri}
+                                : {uri: starImgCornerUri}
+                            }
+                            />
+                            </TouchableOpacity>
+                        )
+                    })
+                }
+            </View>
+        )
+    }
 
     return (
 
@@ -11,6 +47,18 @@ export default () => {
 
             <View>
                 <Text style={styles.title}>CALIFICACIONES</Text>
+
+                <CustomRatingBAr/>
+                <Text style={styles.textStyle}>
+                    {defaultRating + ' / ' + maxRating.length}
+                </Text>
+                <TouchableOpacity
+                activeOpacity={0.7}
+                style={styles.buttonStyle}
+                onPress={() => alert(defaultRating)}
+                >
+                    <Text> Por favor, calificá las canchas</Text>
+                </TouchableOpacity>
             </View>
             </ImageBackground>
         </View>     
@@ -35,4 +83,26 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 30,
       },
+    textStyle: {
+        textAlign: 'center',
+        fontSize: 23,
+        marginTop: 20
+    },
+    customRatingBarStyle: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginTop: 30
+    },
+    starImgStyle: {
+        width: 40,
+        height: 40,
+        resizeMode: 'cover'
+    },
+    buttonStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30,
+        padding: 15,
+        backgroundColor: 'green'
+    }
 });
